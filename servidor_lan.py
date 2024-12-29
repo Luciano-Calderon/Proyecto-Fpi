e47448f36fd614a923c4aea03511906071df7b45
Luciano Calderón <luciano.calderon@usach.cl>
2024-12-29 18:39:52 -0300

    está listo el código del servidor

---
+++ servidor_lan.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+
+
+app=Flask(__name__)
+
+# salas son los datos que vamos a subir al servidor lan, una lista de diccionarios
+salas = [
+    {
+        "computador1":"Encendido",
+        "computador2":"Apagado",
+        "computador3":"Directamente no fucniona"
+    },
+    {
+        "computador1":"Encendido",
+        "computador2":"Encendido",
+        "computador3":"Apagado"
+    }
+]
+
+#con el / indico que es la ruta raíz, yo puedo elegir la ruta que va a tener lo que se muestra
+@app.route("/")
+def informacion():
+    return jsonify(salas)
+
+if __name__=="__main__":
+    #debug=True es para ver los cambios a tiempo real, modo de depuración
+    #el puerto es completamente mi eleción, por defecto es 5000
+    app.run(debug=True, port=5000, host="0.0.0.0")
